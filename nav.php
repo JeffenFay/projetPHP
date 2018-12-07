f7dadfb2ffea560a349d1885726400c92e3dde78
Thibanne <thibault-anne@orange.fr>
2018-12-07 09:45:01 +0100

    Navbar 'reglé'

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
   <div class="row justify-content-center">
     <div class="col-12">
       <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-        <a class="navbar-brand" href="#">Projet PHP</a>
+        <a class="navbar-brand" href="/">Projet PHP</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
